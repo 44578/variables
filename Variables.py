f499805f2c0e58cb4333cb63cfd61330cbaa2165
Hamza Butt <44578@longroad.ac.uk>
2014-09-09 15:09:10 +0100

    Excercise 1.2

---
+++ Variables.py
@@ -0,0 +1,12 @@
+#Hamza Butt
+#09-09-2014
+#Excercise 1.2 - Variables 
+
+print("Hello world")
+#Instead of refering to the = sign as "equals", it is called assigned 
+first_name = input("Please enter your name: ")
+#The first name is a variable, and is created based on the user's input 
+print(first_name)
+#This command prints the variable created earlier
+print("Hi {0}!".format(first_name))
+#Finally, another command is added to the variable which produces a sentence
